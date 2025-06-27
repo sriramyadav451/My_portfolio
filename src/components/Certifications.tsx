39609d5b22876be22315d49b68c9f8059a12a70b
EERLA SRIRAM YADAV <ysriram64@gmail.com>
2025-06-27 18:14:19 +0530

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -8,32 +8,30 @@ const Certifications = () => {
       issuer: "NPTEL (IIT/National Programme on Technology Enhanced Learning)",
       date: "Jan–Apr 2024",
       status: "Completed",
-      description: "12-week course covering core Java programming concepts including OOP, exception       handling, collections, multithreading, and applet development.",
-      credentialId: "NPTEL24CS43S1057401067",
+      description: "12-week course covering core Java programming concepts including OOP, exception       handling, collections, multithreading, and applet development."
     },
     {
       title: "AWS Academy Graduate - AWS Academy Cloud Operations",
       issuer: "AWS Academy",
       date: "March 19, 2025",
       status: "Completed",
-      description: "40-hour course focusing on AWS Cloud operations including monitoring, automation, incident response, and account management using AWS services.",
-      credentialId: "https://www.credly.com/go/wgMRwJEL",
+      description: "40-hour course focusing on AWS Cloud operations including monitoring, automation, incident response, and account management using AWS services."
     },
     {
       title: "AWS Academy Graduate - AWS Academy Cloud Foundations",
       issuer: "AWS Academy",
       date: "March 16, 2025",
       status: "Completed",
-      description: "20-hour introductory course covering AWS core services, security, architecture, pricing, and support.",
-      credentialId: "https://www.credly.com/go/wvxh9Mfg",
+      description: "20-hour introductory course covering AWS core services, security, architecture, pricing, and support."
+      
     },
     {
       title: "Mastering Generative AI and ChatGPT",
       issuer: "GeeksforGeeks",
       date: "2025",
       status: "Completed",
-      description: "5-week course focused on mastering Generative AI concepts and hands-on experience with ChatGPT.",
-      credentialId: "https://media.geeksforgeeks.org/courses/certificates/18facd51d6f8bc2bc0933d904281e811.pdf",
+      description: "5-week course focused on mastering Generative AI concepts and hands-on experience with ChatGPT."
+      
     },
   ];
 
@@ -79,10 +77,6 @@ const Certifications = () => {
                   <div className="flex items-center space-x-1">
                     <Calendar size={16} />
                     <span>{cert.date}</span>
-                  </div>
-                  <div>
-                    <span className="font-medium">ID: </span>
-                    <span className="font-mono">{cert.credentialId}</span>
                   </div>
                 </div>
                 <button className="flex items-center space-x-1 text-blue-600 hover:text-blue-800 transition-colors">
